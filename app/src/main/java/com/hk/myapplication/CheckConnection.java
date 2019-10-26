abcc1a9369c0def9360df09ab3867ba5d3dab5e9
hkobir <hkobir10@gmail.com>
2019-10-27 00:28:48 +0600

    add rciver class for check network state

---
+++ app/src/main/java/com/hk/myapplication/CheckConnection.java
@@ -0,0 +1,23 @@
+package com.hk.myapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class CheckConnection extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        boolean isConnected = networkInfo!=null||networkInfo.isConnectedOrConnecting();
+        if(isConnected){
+            Toast.makeText(context,"Back to the on line")
+        }
+        else{
+
+        }
+    }
+}
