96e29722f914e82243647adedd50b03151473a04
hkobir <hkobir10@gmail.com>
2019-10-27 00:59:53 +0600

    add reciever in manifest

--- app/src/main/java/com/hk/myapplication/CheckConnection.java
+++ app/src/main/java/com/hk/myapplication/CheckConnection.java
@@ -10,9 +10,11 @@ import android.widget.Toast;
 public class CheckConnection extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        ConnectivityManager connectivityManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-        boolean isConnected = networkInfo!=null||networkInfo.isConnectedOrConnecting();
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
         if(isConnected){
             Toast.makeText(context,"Back to the on line",Toast.LENGTH_SHORT).show();
         }
